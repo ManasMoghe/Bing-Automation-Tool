7ae005683bfc6970267c2126e741d00d759111c1
Manas Moghe <mxlieutenant@gmail.com>
2025-06-15 22:05:34 +0530

    add multiple profiles

--- searches1.py
+++ searches1.py
@@ -3,6 +3,7 @@ import time
 
 # Path to Microsoft Edge
 edge_path = r"C:\Program Files (x86)\Microsoft\Edge\Application\msedge.exe"
+profile_dir = ("Profile 2","Profile 1", "Profile 3")
 
 # Your search queries
 queries = ["weather today", "AI news", "python tricks"]
@@ -10,5 +11,5 @@ queries = ["weather today", "AI news", "python tricks"]
 # Open 3 Bing searches
 for query in queries:
     url = f"https://www.bing.com/search?q={query}"
-    subprocess.Popen([edge_path, url])
-    time.sleep(2)  # delay between opening tabs
+    subprocess.Popen([edge_path, f"--profile-directory={profile_dir}", "https://www.bing.com"])
+    time.sleep(2)
