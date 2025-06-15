8f2b0b719bcd9bf3392495bb967f403046e127ca
Manas Moghe <mxlieutenant@gmail.com>
2025-06-15 22:07:10 +0530

    add type seaches

--- searches1.py
+++ searches1.py
@@ -1,15 +1,16 @@
 import subprocess
+import pyautogui
 import time
 
-# Path to Microsoft Edge
 edge_path = r"C:\Program Files (x86)\Microsoft\Edge\Application\msedge.exe"
-profile_dir = ("Profile 2","Profile 1", "Profile 3")
+subprocess.Popen(edge_path)
 
-# Your search queries
-queries = ["weather today", "AI news", "python tricks"]
+time.sleep(5)
 
-# Open 3 Bing searches
-for query in queries:
-    url = f"https://www.bing.com/search?q={query}"
-    subprocess.Popen([edge_path, f"--profile-directory={profile_dir}", "https://www.bing.com"])
-    time.sleep(2)
+pyautogui.click(x=400, y=50)
+
+time.sleep(0.5)
+
+pyautogui.write("weather today", interval=0.1)
+
+pyautogui.press('enter')
